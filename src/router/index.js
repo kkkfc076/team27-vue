98110967ba69256fdc1a5ceda1496c4f043b8431
坤凡 陈 <535742729@qq.com>
2022-06-24 09:54:33 +0800

    改登录

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
+import Manager from '@/components/Manager'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/manager',
+      name: 'Manager',
+      component: Manager
     }
   ]
 })
