1a4bf6d94469ac3f87fc3a17a2bd53ea166eda4f
坤凡 陈 <535742729@qq.com>
2022-06-25 20:31:56 +0800

    提交申请前端

--- src/page/student/main/Ap.vue
+++ src/page/student/main/Ap.vue
@@ -8,11 +8,11 @@
    贫困等级:{{userList.plevel}}<br>
    批次:{{userList.pyear}}<br>
    <el-form ref="form" :model="form" label-width="10000xp">
-     <el-form-item label="申请理由">
+     <el-form-item label="申请理由" prop="reason">
        <el-input type="textarea" v-model="form.reason"></el-input>
      </el-form-item>
      <el-form-item>
-       <el-button type="primary" >提交申请</el-button>
+       <el-button type="primary" @click="submit()" >提交申请</el-button>
        <el-button>取消</el-button>
      </el-form-item>
    </el-form>
@@ -39,6 +39,18 @@ export default {
       this.$axios.get(`/api/student/getInfo`).then(res => {
         console.info(res.data)
         this.userList = res.data.data
+      })
+    },
+    submit () {
+      var reason = this.form.reason
+      this.$axios.post('/api/applicationform/addReason', {reason}).then(res => {
+        console.assert(res.data.data)
+        if (res.data.data.flag === 2) {
+          alert('提交成功!')
+        } else {
+          alert(' 错误！')
+          return false
+        }
       })
     }
   }
