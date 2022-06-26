98d2f1936821be35008ef57b0add462f01dc9f5c
Administrator <2854921685@qq.com>
2022-06-26 19:43:35 +0800

    学生寒衣登记界面

--- src/api/waitforcheckList.js
+++ src/api/waitforcheckList.js
@@ -1,3 +1,4 @@
 import http from '../tools/http/http'
 export const waitForList = (query) => http.GET('/api/managerApplication/pageList', query)
 export const checkedList = (query) => http.GET('/api/managerApplication/checkedPageList', query)
+//
