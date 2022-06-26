52a5d78c319c63750409365241249675cc4b5af7
yuhanqi <1790560630@qq.com>
2022-06-26 19:11:52 +0800

    新增申请、记录查看

--- src/api/manList.js
+++
@@ -1,5 +0,0 @@
-import http from '../tools/http/http'
-// 分页查询所有管理员
-export const manList = (query) => http.GET('/api/manager/getManlist', query)
-// 批量授权
-export const setPermissions = (query) => http.POST('/api/manager/setPermission', query)
